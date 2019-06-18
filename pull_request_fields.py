c2826f4296554c996c15eec7e4e0be33af3e534e
Lennox Stevenson <lennox.stevenson@mail.utoronto.ca>
2019-06-17 23:07:34 -0400

    feat: fill out logic for pull request fields

--- pull_request_fields.py
+++ pull_request_fields.py
@@ -1,14 +1,47 @@
 from datetime import date
+import re
 
 
-def title():
-    return "Release {today}".format(today=date.today())
+def is_merge_commit(message):
+    message_regex = "^Merge pull request #(\d+).*\n\n(.*)$"
+    message_pattern = re.compile(merge_commit_message_regex)
+
+    return message_pattern.match(message)
 
 
-def pull_request_fields(repository):
-    body = ""
-    base = ""
-    head = ""
+def commit_changelog_message(commit):
+    return "* lennoxstevenson: #1234 - Pull Request title"
 
-    return {"title": title(), "body": body, "base": base, "head": head}
+
+def __pull_request_body(repository, base, head):
+    print()
+
+    compare = repository.compare(base, head)
+
+    print(compare.commits[0].commit.message)
+
+    merge_commits = [
+        commit
+        for commit in compare.commits
+        if is_merge_commit_message(commit.commit.message)
+    ]
+
+    print(merge_commits)
+
+    pull_request_body_lines = ["# Changelog"]
+
+    pull_request_body_lines.extend(
+        [commit_changelog_message(commit) for commit in merge_commits]
+    )
+
+    return "\n".join(pull_request_body_lines)
+
+
+def pull_request_fields(repository, head):
+    title = "Release {today}".format(today=date.today())
+    base = "master"
+
+    body = __pull_request_body(repository, base, head)
+
+    return {"title": title, "body": body, "base": base, "head": head}
 
--- tests/test_pull_request_fields.py
+++ tests/test_pull_request_fields.py
@@ -1,20 +1,79 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, PropertyMock
 from datetime import date
 
 import pull_request_fields
 
 
+def mock_repository():
+    message_mock = PropertyMock()
+    message = "Merge pull request #1 from somewhere.\n\nPull Request title"
+    message_mock.return_value = message
+
+    git_commit_mock = MagicMock()
+    git_commit_mock.message = message_mock
+
+    commit_mock = MagicMock()
+    commit_mock.commit = git_commit_mock
+
+    compare_mock = MagicMock()
+    compare_mock.commits = [commit_mock]
+
+    repository_mock = MagicMock()
+    repository_mock.compare = compare_mock
+
+    return repository_mock
+
+
 class TestPullRequestFields(unittest.TestCase):
     @patch("pull_request_fields.date")
     def test_title(self, date_mock):
+        repository_mock = mock_repository()
 
         date_mock.today.return_value = date(2019, 6, 13)
         date_mock.side_effect = lambda *args, **kw: date(*args, **kw)
 
-        result = pull_request_fields.pull_request_fields(None)
+        result = pull_request_fields.pull_request_fields(repository_mock, None)
 
         self.assertEqual(result["title"], "Release 2019-06-13")
+
+    def test_base(self):
+        repository_mock = mock_repository()
+
+        result = pull_request_fields.pull_request_fields(repository_mock, None)
+
+        self.assertEqual(result["base"], "master")
+
+    def test_head(self):
+        repository_mock = mock_repository()
+
+        result = pull_request_fields.pull_request_fields(
+            repository_mock, "release-2019-06-14-a1b2c3d4"
+        )
+
+        self.assertEqual(result["head"], "release-2019-06-14-a1b2c3d4")
+
+    def test_body_no_commits(self):
+        repository_mock = mock_repository()
+
+        result = pull_request_fields.pull_request_fields(repository_mock, None)
+
+        self.assertEqual(result["body"], "# Changelog")
+
+    def test_body_one_commit(self):
+        repository_mock = mock_repository()
+
+        result = pull_request_fields.pull_request_fields(repository_mock, None)
+
+        self.assertEqual(
+            result["body"], "# Changelog\n* lennoxstevenson: #1234 - Pull Request title"
+        )
+
+    def test_body_multiple_commits_one_pull_request(self):
+        pass
+
+    def test_body_multiple_commits_multiple_pull_requests(self):
+        pass
 
 
 if __name__ == "__main__":
